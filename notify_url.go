3098fe1459beb8be656a801447c72e356d36148e
dtapps <li.gc@icloud.com>
2024-02-26 12:32:14 +0800

    - 优化通知回调地址

--- notify_url.go
+++ notify_url.go
@@ -16,20 +16,8 @@ type NotifyUrlParams struct {
 	ApiKey    string `json:"api_key"`    // 秘钥
 }
 
-type NotifyUrlRequestBody struct {
-	RechargeAccount string  `json:"recharge_account"` // 充值账号
-	RechargeMoney   float64 `json:"recharge_money"`   // 充值金额 单位：元
-	RechargeType    string  `json:"recharge_type"`    // 充值类型
-	RechargeReason  string  `json:"recharge_reason"`  // 充值失败原因 只有充值失败才会返回的数据内容
-	OrderID         string  `json:"order_id"`         // 订单编号
-	OrderNo         string  `json:"order_no"`         // 商户订单编号
-	OrderRemarks    string  `json:"order_remarks"`    // 订单备注
-	OrderStatus     string  `json:"order_status"`     // 订单状态 SUCCESS=充值成功，FAILURE=充值失败，RECHARGE=充值中
-	OrderCost       float64 `json:"order_cost"`       // 订单成本价 单位：元，只有充值成功才会返回的数据内容
-}
-
 // NotifyUrl 通知回调地址
-func (c *Client) NotifyUrl(ctx context.Context, params NotifyUrlParams, requestBody NotifyUrlRequestBody) error {
+func (c *Client) NotifyUrl(ctx context.Context, params NotifyUrlParams, param gorequest.Params) error {
 
 	// 验证回调地址
 	_, err := url.ParseRequestURI(params.NotifyUrl)
@@ -46,19 +34,7 @@ func (c *Client) NotifyUrl(ctx context.Context, params NotifyUrlParams, requestB
 	XTimestamp := fmt.Sprintf("%v", gotime.Current().Timestamp())
 
 	// 签名
-	requestParams := gorequest.NewParams()
-	requestParams.Set("recharge_account", requestBody.RechargeAccount)
-	requestParams.Set("recharge_money", requestBody.RechargeMoney)
-	requestParams.Set("recharge_type", requestBody.RechargeType)
-	requestParams.Set("recharge_reason", requestBody.RechargeReason)
-	requestParams.Set("order_id", requestBody.OrderID)
-	requestParams.Set("order_no", requestBody.OrderNo)
-	requestParams.Set("order_remarks", requestBody.OrderRemarks)
-	requestParams.Set("order_status", requestBody.OrderStatus)
-	requestParams.Set("order_cost", requestBody.OrderCost)
-
-	// 签名
-	XSign := sign(requestParams, params.ApiKey, XTimestamp)
+	XSign := sign(param, params.ApiKey, XTimestamp)
 
 	// 创建请求
 	client := gorequest.NewHttp()
@@ -70,7 +46,7 @@ func (c *Client) NotifyUrl(ctx context.Context, params NotifyUrlParams, requestB
 	client.SetContentTypeJson()
 
 	// 设置参数
-	client.SetParams(requestParams)
+	client.SetParams(param)
 
 	// 添加请求头
 	client.SetHeader("X-Timestamp", XTimestamp)
